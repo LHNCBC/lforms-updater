10595575ae8ef090f4191fa091a8c5959ce36a44
Paul Lynch <plynch@mail.nih.gov>
2020-03-13 10:57:31 -0400

    Added a browser-ready version (in the "browser" directory)

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+module.exports = {
+  node: {
+    fs: "empty"
+  },
+  entry: {
+    main: [
+//      '@babel/polyfill',
+      './source/index.js'
+    ]
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    path: __dirname,
+    filename: 'browser/updater.js',
+    libraryTarget: 'assign',
+    library: "lformsUpdater"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
