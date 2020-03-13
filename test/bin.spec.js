9adab1cb0ce5563fc62a6fea57209b061b076a4c
Paul Lynch <plynch@mail.nih.gov>
2020-03-13 11:10:20 -0400

    Removed test focus

--- test/bin.spec.js
+++ test/bin.spec.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const path = require('path');
 
 describe('cli', ()=>{
-  it.only('should update an LForms form', ()=>{
+  it('should update an LForms form', ()=>{
     let cliPath = path.join(__dirname, '../bin/updater.js');
     let dataPath = path.join(__dirname, './22.0.0/weightHeightLForms.json');
     let results = require('child_process').execSync(cliPath +' -f ' + dataPath);
