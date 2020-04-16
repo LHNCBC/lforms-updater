eb2b358617d03ce51e7713c8513f8d1d1a946dba
Ye Wang <wangye@nih.gov>
2020-04-16 15:44:04 -0400

    Revert "update tests"
    This reverts commit 3c6d9c42414d61f579da8c3cda1fdfdb9bf41b66.

--- test/23.0.0/spec.js
+++ test/23.0.0/spec.js
@@ -8,116 +8,99 @@ const UPDATE_VERSION = '23.0.0';
 const WRONG_URI = "http://hl7.org/fhir/StructureDefinition/questionnaire-answerRepeats";
 let updateVersionTag_ = util.makeVersionTag(UPDATE_VERSION);
 
-if (!global.window)
-  global.window = {}; // The browser version assigns to window
-require(path.join(__dirname, '../../browser/updater'));
+describe(UPDATE_VERSION, function() {
+  describe('version tag', function() {
+    it('should be moved from "display" to "code"', ()=>{
+      let obsDefData = fs.readFileSync(
+        path.join(__dirname, './obs.json')).toString();
+      let obsDef = JSON.parse(obsDefData);
+      assert(obsDef.meta.tag[0].display); // precondition check
+      let updatedObs = updater.update(obsDef, UPDATE_VERSION);
+      assert(!obsDef.meta.tag[0].display);
+      assert.equal(obsDef.meta.tag[0].code, updateVersionTag_);
+    });
+  });
 
-const testCases = [
-  {"test": "Binary version", "updater": updater},
-  {"test": "Browser version", "updater": window.lformsUpdater}
-];
+  describe('Questionnaire', function() {
+    let qDefData, qDef;
+    before(() => {
+      qDefData = fs.readFileSync(
+          path.join(__dirname, './weightHeightQ.json')).toString();
+    });
 
-for (let i=0, len=testCases.length; i<len; ++i) {
-  (function (testCase) {
-    describe(testCase.test, function() {
-      describe(UPDATE_VERSION, function() {
-        describe('version tag', function() {
-          it('should be moved from "display" to "code"', ()=>{
-            let obsDefData = fs.readFileSync(
-                path.join(__dirname, './obs.json')).toString();
-            let obsDef = JSON.parse(obsDefData);
-            assert(obsDef.meta.tag[0].display); // precondition check
-            let updatedObs = testCase.updater.update(obsDef, UPDATE_VERSION);
-            assert(!obsDef.meta.tag[0].display);
-            assert.equal(obsDef.meta.tag[0].code, updateVersionTag_);
-          });
-        });
-
-        describe('Questionnaire', function() {
-          let qDefData, qDef;
-          before(() => {
-            qDefData = fs.readFileSync(
-                path.join(__dirname, './weightHeightQ.json')).toString();
-          });
-
-          describe('updated unversioned Questionnaire', () => {
-            let revised, weightExt, heightExt;
-            before(() => {
-              qDef = JSON.parse(qDefData);
-              revised = testCase.updater.update(qDef, UPDATE_VERSION);
-              weightExt = revised.item[0].extension;
-              heightExt = revised.item[1].extension;
-            });
-            it('should still have questionnaire-unit', () => {
-              assert.equal(weightExt[0].url,
-                  "http://hl7.org/fhir/StructureDefinition/questionnaire-unit");
-            });
-            it('should have removed the answer-repeats extension', ()=> {
-              assert.equal(weightExt.length, 1);
-              assert.equal(heightExt.length, 1);
-              assert.equal(revised.item[0].repeats, true);
-              assert.equal(revised.item[1].repeats, true);
-            });
-            it('should have the LForms version updated', ()=>{
-              assert.equal(revised.meta.tag[0].code, updateVersionTag_);
-            });
-          });
-
-
-          describe('old versioned Questionnaire', () => {
-            let revised, weightExt, heightExt;
-            before(() => {
-              qDef = JSON.parse(qDefData);
-              qDef.meta.tag = [{display: 'lformsVersion: 21.2.1'}];
-              revised = testCase.updater.update(qDef, UPDATE_VERSION);
-              weightExt = revised.item[0].extension;
-              heightExt = revised.item[1].extension;
-            });
-
-            it('should have updated URLs for the answer-repeats extension', ()=> {
-              assert.equal(weightExt.length, 1);
-              assert.equal(heightExt.length, 1);
-              assert.equal(revised.item[0].repeats, true);
-              assert.equal(revised.item[1].repeats, true);
-            });
-            it('should have the LForms version updated', ()=>{
-              assert.equal(revised.meta.tag[0].code, updateVersionTag_);
-            });
-          });
-
-
-          describe('future versioned Questionnaire', () => {
-            // This tests that we don't run an update for a version that should
-            // already have the update (if the version number is later than the
-            // version number that requires it).
-            let revised, weightExt, versionTag='lformsVersion: 25.2.1';
-            before(() => {
-              qDef = JSON.parse(qDefData);
-              qDef.meta.tag = [{display: versionTag}];
-              revised = testCase.updater.update(qDef, UPDATE_VERSION);
-              weightExt = revised.item[0].extension;
-              heightExt = revised.item[1].extension;
-            });
-
-            it('should not have updated URLs for answer-repeats extension', ()=> {
-              // If this were really a future-versioned Questionnaire, the URI would
-              // already be updated, so the updater should have left them untouched.
-              assert.equal(weightExt[1].url, WRONG_URI);
-              assert.equal(heightExt[1].url, WRONG_URI);
-              assert.equal(revised.item[0].repeats, undefined);
-              assert.equal(revised.item[1].repeats, undefined);
-            });
-            it('should not have the LForms version updated', ()=>{
-              assert.equal(revised.meta.tag[0].display, versionTag);
-            });
-          });
-        });
-
+    describe('updated unversioned Questionnaire', () => {
+      let revised, weightExt, heightExt;
+      before(() => {
+        qDef = JSON.parse(qDefData);
+        revised = updater.update(qDef, UPDATE_VERSION);
+        weightExt = revised.item[0].extension;
+        heightExt = revised.item[1].extension;
       });
-    })
-  })(testCases[i])
-}
+      it('should still have questionnaire-unit', () => {
+        assert.equal(weightExt[0].url,
+            "http://hl7.org/fhir/StructureDefinition/questionnaire-unit");
+      });
+      it('should have removed the answer-repeats extension', ()=> {
+        assert.equal(weightExt.length, 1);
+        assert.equal(heightExt.length, 1);
+        assert.equal(revised.item[0].repeats, true);
+        assert.equal(revised.item[1].repeats, true);
+      });
+      it('should have the LForms version updated', ()=>{
+        assert.equal(revised.meta.tag[0].code, updateVersionTag_);
+      });
+    });
 
 
+    describe('old versioned Questionnaire', () => {
+      let revised, weightExt, heightExt;
+      before(() => {
+        qDef = JSON.parse(qDefData);
+        qDef.meta.tag = [{display: 'lformsVersion: 21.2.1'}];
+        revised = updater.update(qDef, UPDATE_VERSION);
+        weightExt = revised.item[0].extension;
+        heightExt = revised.item[1].extension;
+      });
+
+      it('should have updated URLs for the answer-repeats extension', ()=> {
+        assert.equal(weightExt.length, 1);
+        assert.equal(heightExt.length, 1);
+        assert.equal(revised.item[0].repeats, true);
+        assert.equal(revised.item[1].repeats, true);
+      });
+      it('should have the LForms version updated', ()=>{
+        assert.equal(revised.meta.tag[0].code, updateVersionTag_);
+      });
+    });
+
+
+    describe('future versioned Questionnaire', () => {
+      // This tests that we don't run an update for a version that should
+      // already have the update (if the version number is later than the
+      // version number that requires it).
+      let revised, weightExt, versionTag='lformsVersion: 25.2.1';
+      before(() => {
+        qDef = JSON.parse(qDefData);
+        qDef.meta.tag = [{display: versionTag}];
+        revised = updater.update(qDef, UPDATE_VERSION);
+        weightExt = revised.item[0].extension;
+        heightExt = revised.item[1].extension;
+      });
+
+      it('should not have updated URLs for answer-repeats extension', ()=> {
+        // If this were really a future-versioned Questionnaire, the URI would
+        // already be updated, so the updater should have left them untouched.
+        assert.equal(weightExt[1].url, WRONG_URI);
+        assert.equal(heightExt[1].url, WRONG_URI);
+        assert.equal(revised.item[0].repeats, undefined);
+        assert.equal(revised.item[1].repeats, undefined);
+      });
+      it('should not have the LForms version updated', ()=>{
+        assert.equal(revised.meta.tag[0].display, versionTag);
+      });
+    });
+  });
+
+});
 
 
--- test/24.0.0/spec.js
+++ test/24.0.0/spec.js
@@ -4,80 +4,67 @@ const path = require('path');
 const updater = require('../../source/index');
 const util = require('../../source/util');
 
-if (!global.window)
-  global.window = {}; // The browser version assigns to window
-require(path.join(__dirname, '../../browser/updater'));
-
 const UPDATE_VERSION = '24.0.0';
-const testCases = [
-  {"test": "Binary version", "updater": updater},
-  {"test": "Browser version", "updater": window.lformsUpdater}
-];
 
-for (let i=0, len=testCases.length; i<len; ++i) {
-  (function (testCase) {
-    describe(testCase.test, ()=> {
-      describe('linkId', function() {
-        let allInOneDef, updatedAllInOneDef;
-        beforeEach(() => {
-          let allInOne = fs.readFileSync(
-              path.join(__dirname, './allInOne.json')).toString();
-          allInOneDef = JSON.parse(allInOne);
-        });
+describe(UPDATE_VERSION, function() {
+  describe('linkId', function() {
+    let allInOneDef, updatedAllInOneDef;
+    beforeEach(() => {
+      let allInOne = fs.readFileSync(
+          path.join(__dirname, './allInOne.json')).toString();
+      allInOneDef = JSON.parse(allInOne);
+    });
 
-        it('should have set a correct version', ()=>{
-          updatedAllInOneDef = testCase.updater.update(allInOneDef, UPDATE_VERSION);
-          assert.equal(updatedAllInOneDef.lformsVersion, UPDATE_VERSION);
-        });
+    it('should have set a correct version', ()=>{
+      updatedAllInOneDef = updater.update(allInOneDef, UPDATE_VERSION);
+      assert.equal(updatedAllInOneDef.lformsVersion, UPDATE_VERSION);
+    });
 
-        it('should generate linkId on each item', () => {
-          updatedAllInOneDef = testCase.updater.update(allInOneDef, UPDATE_VERSION);
-          updatedAllInOneDef.items.forEach( item => {
-            assert(item.linkId)
-          })
-        });
+    it('should generate linkId on each item', () => {
+      updatedAllInOneDef = updater.update(allInOneDef, UPDATE_VERSION);
+      updatedAllInOneDef.items.forEach( item => {
+        assert(item.linkId)
+      })
+    });
 
-        it('should convert skip logic to use linkId', () => {
-          assert.equal(allInOneDef.items[36].linkId, undefined);
-          assert.equal(allInOneDef.items[36].skipLogic.conditions[0].source, "slALLSource1");
-          assert.equal(allInOneDef.items[36].skipLogic.conditions[1].source, "slALLSource2");
-          assert.equal(allInOneDef.items[41].items[1].skipLogic.conditions[0].source, "rpSource1");
+    it('should convert skip logic to use linkId', () => {
+      assert.equal(allInOneDef.items[36].linkId, undefined);
+      assert.equal(allInOneDef.items[36].skipLogic.conditions[0].source, "slALLSource1");
+      assert.equal(allInOneDef.items[36].skipLogic.conditions[1].source, "slALLSource2");
+      assert.equal(allInOneDef.items[41].items[1].skipLogic.conditions[0].source, "rpSource1");
 
-          updatedAllInOneDef = testCase.updater.update(allInOneDef, UPDATE_VERSION);
+      updatedAllInOneDef = updater.update(allInOneDef, UPDATE_VERSION);
 
-          assert.equal(updatedAllInOneDef.items[36].linkId, "/slALLTargetItem");
-          assert.equal(updatedAllInOneDef.items[36].skipLogic.conditions[0].source, "/slALLSource1");
-          assert.equal(updatedAllInOneDef.items[36].skipLogic.conditions[1].source, "/slALLSource2");
+      assert.equal(updatedAllInOneDef.items[36].linkId, "/slALLTargetItem");
+      assert.equal(updatedAllInOneDef.items[36].skipLogic.conditions[0].source, "/slALLSource1");
+      assert.equal(updatedAllInOneDef.items[36].skipLogic.conditions[1].source, "/slALLSource2");
 
-          assert.equal(updatedAllInOneDef.items[41].items[1].skipLogic.conditions[0].source, "/repeatingSection1/rpSource1");
-        });
+      assert.equal(updatedAllInOneDef.items[41].items[1].skipLogic.conditions[0].source, "/repeatingSection1/rpSource1");
+    });
 
-        it('should convert data control to use linkId', () => {
-          assert.equal(allInOneDef.items[42].items[1].dataControl[0].source.sourceItemCode, "itemWithExtraData");
+    it('should convert data control to use linkId', () => {
+      assert.equal(allInOneDef.items[42].items[1].dataControl[0].source.sourceItemCode, "itemWithExtraData");
 
-          updatedAllInOneDef = testCase.updater.update(allInOneDef, UPDATE_VERSION);
+      updatedAllInOneDef = updater.update(allInOneDef, UPDATE_VERSION);
 
-          assert.equal(updatedAllInOneDef.items[42].items[1].dataControl[0].source.sourceLinkId, "/dataControlExamples/itemWithExtraData");
-
-        });
-
-        it('should convert calculation method (BMI) to use linkId', () => {
-          assert.equal(allInOneDef.items[57].calculationMethod.name, "BMI");
-          assert.equal(allInOneDef.items[57].calculationMethod.value[0], "3141-9");
-          assert.equal(allInOneDef.items[57].calculationMethod.value[1], "8302-2");
-
-          updatedAllInOneDef = testCase.updater.update(allInOneDef, UPDATE_VERSION);
-
-          assert.equal(allInOneDef.items[57].calculationMethod.name, "BMI");
-          assert.equal(allInOneDef.items[57].calculationMethod.value[0], "/3141-9");
-          assert.equal(allInOneDef.items[57].calculationMethod.value[1], "/8302-2");
-        });
-
-      });
+      assert.equal(updatedAllInOneDef.items[42].items[1].dataControl[0].source.sourceLinkId, "/dataControlExamples/itemWithExtraData");
 
     });
 
-  })(testCases[i])
-}
+    it('should convert calculation method (BMI) to use linkId', () => {
+      assert.equal(allInOneDef.items[57].calculationMethod.name, "BMI");
+      assert.equal(allInOneDef.items[57].calculationMethod.value[0], "3141-9");
+      assert.equal(allInOneDef.items[57].calculationMethod.value[1], "8302-2");
+
+      updatedAllInOneDef = updater.update(allInOneDef, UPDATE_VERSION);
+
+      assert.equal(allInOneDef.items[57].calculationMethod.name, "BMI");
+      assert.equal(allInOneDef.items[57].calculationMethod.value[0], "/3141-9");
+      assert.equal(allInOneDef.items[57].calculationMethod.value[1], "/8302-2");
+    });
+
+  });
+
+});
 
 
