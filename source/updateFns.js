9e174a92cc8ffcf2c6cc0126698c26a6893ed55a
Paul Lynch <plynch@mail.nih.gov>
2021-11-02 19:41:37 -0400

    Removed dynamic export

---
+++ source/updateFns.js
@@ -0,0 +1,8 @@
+module.exports = {};
+module.exports['29.0.0'] = require('./versionUpdates/29.0.0.js');
+module.exports['28.0.0'] = require('./versionUpdates/28.0.0.js');
+module.exports['26.0.0'] = require('./versionUpdates/26.0.0.js');
+module.exports['25.0.0'] = require('./versionUpdates/25.0.0.js');
+module.exports['24.0.0'] = require('./versionUpdates/24.0.0.js');
+module.exports['23.0.0'] = require('./versionUpdates/23.0.0.js');
+module.exports['22.0.0'] = require('./versionUpdates/22.0.0.js');
